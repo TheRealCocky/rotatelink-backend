e62d1225083f6c0aee929ad0cc211fe4781b31f4
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2025-09-18 23:49:22 +0100

    fixing url issues

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,12 +7,12 @@ import { LoginDto } from './dto/login.dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('register')
+  @Post('signup')
   register(@Body() dto: RegisterDto) {
     return this.authService.register(dto.username, dto.password);
   }
 
-  @Post('login')
+  @Post('signin')
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto.username, dto.password);
   }
