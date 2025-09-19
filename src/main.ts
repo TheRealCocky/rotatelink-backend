e0f013580bcc0a506304347b4a583398f8c680f2
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2025-09-19 23:36:48 +0100

    actualizando o main da app over again :(

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,22 @@ async function bootstrap() {
 
   // Configuração de CORS explícita
   app.enableCors({
-    origin: [
-      'http://localhost:3000', // dev local
-      'https://rotatelink-frontend-v1yw.vercel.app', // frontend produção
-    ],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'https://rotatelink-frontend-v1yw.vercel.app',
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin); // devolve o mesmo origin
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
+
 
   const port = Number(process.env.PORT) || 3001;
   await app.listen(port);
